012a1ad48f1143757189bb14bcba41a66356ce4d
pratyay <pganguli5520@gmail.com>
2025-08-04 23:57:17 +0530

    added json v2 examples

--- golang/features/1_25/api.go
+++ golang/features/1_25/api.go
@@ -1,6 +1,15 @@
 package features
 
-// we will be using the same struct for most of the examples in feature comparison examples
+import (
+	"encoding/json/jsontext"
+	"encoding/json/v2"
+	"log"
+
+	"github.com/brianvoe/gofakeit/v6"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+//we will be using the same struct for most of the examples in feature comparison examples
 
 type Gender string
 
@@ -19,30 +28,53 @@ var (
 
 // These are the set of data which we will be collecting at the time of registration.
 type User struct {
-	Id          string      `json:"id"`
-	Name        string      `json:"name"`
-	Age         uint8       `json:"age"`
-	Gender      Gender      `json:"gender"`
-	BloodGroup  BloodGroup  `json:"bloodGroup"`
-	Phone       string      `json:"phone"`
-	UserProfile UserProfile `json:"userProfile"`
+	Id          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name        string             `json:"name"`
+	Age         uint8              `json:"age"`
+	Gender      Gender             `json:"gender"`
+	BloodGroup  BloodGroup         `json:"bloodGroup"`
+	Phone       string             `json:"phone"`
+	UserProfile *UserProfile       `json:"userProfile"`
 }
 
 // Metadata related to user
 type UserProfile struct {
-	Id             string `json:"id"`
-	Address        string `json:"address"`
-	ProfilePicture string `json:"profilePicture"`
-	EmailAddress   string `json:"emailAddress"`
+	Id             primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Address        string             `json:"address"`
+	ProfilePicture string             `json:"profilePicture"`
+	EmailAddress   string             `json:"emailAddress"`
 }
 
 // Validate all the data related to patient at the time of onboarding.
-func (p *User) Validate() {
+func (u *User) Validate() {
+}
+
+func (u *User) CreateDummy() {
+	u = &User{
+		Id:   primitive.NewObjectID(),
+		Name: gofakeit.Name(),
+		// ommit check
+		//Age:    gofakeit.Uint8(),
+		Gender: Male,
+		Phone:  gofakeit.Phone(),
+		// ommit check for struct pointers
+		// UserProfile: &UserProfile{
+		// 	Id:             primitive.NewObjectID(),
+		// 	Address:        gofakeit.Address().Address,
+		// 	ProfilePicture: gofakeit.Person().Image,
+		// 	EmailAddress:   gofakeit.Email(),
+		// },
+	}
+	if byteArr, err := json.Marshal(u, json.OmitZeroStructFields(true), jsontext.WithIndent("\t")); err == nil {
+		log.Printf("Encoded res : %s", (string(byteArr)))
+	}
 }
 
 func (u *User) Create() {
+	//business logic here
 }
 
 // Create users in batches, sort of a bulk creation function...
 func CreateBatchUser([]User) {
+	//business logic here
 }
--- golang/features/go.mod
+++ golang/features/go.mod
@@ -1,3 +1,8 @@
 module github.com/pratyayganguli/backend/golang/features
 
 go 1.24.5
+
+require (
+	github.com/brianvoe/gofakeit/v6 v6.28.0
+	go.mongodb.org/mongo-driver v1.17.4
+)
