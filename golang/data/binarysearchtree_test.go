d1f6640103c73848fce0a2d224035f0b331f06c6
pratyay <pganguli5520@gmail.com>
2025-08-14 20:15:20 +0530

    wip code from the golang data structure session - I

---
+++ golang/data/binarysearchtree_test.go
@@ -0,0 +1,48 @@
+package data
+
+import (
+	"fmt"
+	"testing"
+)
+
+// todo: make it non-generic and then implement the generic part.
+type BSTNode struct {
+	Data  int
+	Left  *BSTNode
+	Right *BSTNode
+}
+
+var (
+	RootNode    *BSTNode
+	CurrentNode *BSTNode
+	// using constants for better readability
+	LeftDirection  string = "left"
+	RightDirection string = "right"
+)
+
+func (n *BSTNode) Insert(data int, direction string) *BSTNode {
+	n.Data = data
+	if direction == LeftDirection {
+		n.Left = &BSTNode{}
+		CurrentNode = n.Left
+		return n.Left
+	} else {
+		n.Right = &BSTNode{}
+		CurrentNode = n.Right
+		return n.Right
+	}
+}
+
+func (n *BSTNode) IsEmptyBSTTree() bool {
+	if RootNode == nil {
+		fmt.Println("root node is empty")
+		return true
+	}
+	return false
+}
+
+func IsEmptyBSTTreeTest(t *testing.T) {
+
+}
+
+// there needs to be recursive call
