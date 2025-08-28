fa9bc9e3daa19ca4df1bffcb432742a97dc20859
pratyay <pganguli5520@gmail.com>
2025-08-28 11:59:29 +0530

    plaindrome question added

--- golang/data/palindrome_test.go
+++ golang/data/palindrome_test.go
@@ -8,51 +8,35 @@ import (
 func TestPalindrome(t *testing.T) {
 	// aaa -> mid = 3/2 = 1
 	// aabb -> mid 4/2 = 2
-	if isPalindrome("hlh") {
-
+	if isPalindrome("xy") {
+		fmt.Println("palindrome")
 	} else {
-		//
+		fmt.Println("not palindrome")
 	}
-	trashCode()
 }
 
-func TestTrashCode(t *testing.T) {
-	trashCode()
-}
-
-func trashCode() {
-	// for odd case
-	// message := "olllo"
-	// for even case
-	// need to handle is different for odd and even lengths...
-	message := "ollo"
-	mid := len(message) / 2
-	if len(message)%2 == 1 {
-		prefix := message[0:mid]
-		suffix := message[mid+1:]
-		fmt.Println(prefix, " ", suffix)
-	} else {
-		prefix := message[0:mid]
-		suffix := message[mid:]
-		fmt.Println(prefix, " ", suffix)
+func comparison(prefix, suffix string) bool {
+	prefixByteArr := []byte(prefix)
+	suffixByteArr := []byte(suffix)
+	j := 0
+	for i := len(prefixByteArr) - 1; i >= 0; i-- {
+		if prefixByteArr[i] != suffixByteArr[j] {
+			return false
+		}
+		j++
 	}
+	return true
 }
 
 func isPalindrome(input string) bool {
-	charArr := []byte(input)
-	if len(charArr) == 0 {
-		fmt.Println("This is an empty string")
-	}
-
-	len := len(charArr)
-	if len/2 < 1 {
-		// only one char is there in the string
-		return true
+	mid := len(input) / 2
+	if len(input)%2 == 1 {
+		prefix := input[0:mid]
+		suffix := input[mid+1:]
+		return comparison(prefix, suffix)
 	} else {
-		if len/2%2 == 0 {
-			return false
-		}
+		prefix := input[0:mid]
+		suffix := input[mid:]
+		return comparison(prefix, suffix)
 	}
-	return false
-
 }
