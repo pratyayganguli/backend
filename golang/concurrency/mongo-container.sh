79ec504cac2bf752ed00346e3be4f845453ff4b8
pratyay <pganguli5520@gmail.com>
2025-09-12 19:38:57 +0530

    added mongo connection starter code

---
+++ golang/concurrency/mongo-container.sh
@@ -0,0 +1,1 @@
+docker run -d --name local-mongo -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=cidaas -e MONGO_INITDB_ROOT_PASSWORD=cidass@000 -e MONGO_INITDB_ROOT_DATABASE=cidaas-db mongo:latest
---
+++ golang/concurrency/mongo_connections_test.go
@@ -0,0 +1,7 @@
+package concurrency
+
+import "testing"
+
+func TestCreateMongoConnection(t *testing.T) {
+	CreateSimpleMongoConnection()
+}
